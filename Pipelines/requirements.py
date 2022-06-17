65d71beee221db67947e27e5cded7adcc1e2bdfd
Mike  Wu <mikewu@Mikes-MacBook-Air.local>
2022-06-17 17:47:43 -0500

    updates

---
+++ Pipelines/requirements.py
@@ -0,0 +1,36 @@
+
+from ipaddress import summarize_address_range
+import math
+import string
+
+
+moviepy==1.0.3
+python-pptx==0.6.21
+pytube==12.1.0
+youtube_transcript_api==0.4.4
+Spacy==2.3.7
+pandas 
+numpy
+glob
+os
+SpeechRecognition==3.8.1
+en-core-web-sm==3.3.0
+torch
+gensim==4.2.0
+# pip install -U git+https://github.com/PrithivirajDamodaran/Gramformer.git
+gramformer==1.0
+en_core_web_lg
+en_core_web_sm
+seaborn
+wordcloud
+nltk
+pickle
+sklearn
+textblob
+math
+summa 
+multiprocessing
+pyLDAvis
+string
+sentence_transformers
+faiss
